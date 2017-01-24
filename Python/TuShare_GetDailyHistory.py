d13e6039d113c7286353fa7ec289629c8150cf05
freefrom <freefrom_mn@163.com>
2017-01-24 11:54:55 +0800

    Refines TuShare_GetDailyHistory.py so that it receives a list of stock ids.

--- Python/TuShare_GetDailyHistory.py
+++ Python/TuShare_GetDailyHistory.py
@@ -13,25 +13,27 @@ import tushare as ts
 #
 # Get Daily History Parameters
 #
-stock_id   = '000300'
-is_index   = True
-start_date = '2005-04-08'
-end_date   = '2016-12-30'
+stock_ids  = ['000916']
+is_indexs  = [True]
+start_date = '2007-07-02'
+end_date   = '2016-06-13'
+path_datacenter = '../DataCenter/'
 
-#
-# Fetch Stock Data
-#
-df = ts.get_stock_basics()
-#start_date = df.ix[stock_id]['timeToMarket'] #上市日期YYYYMMDD
-
-df = ts.get_h_data(stock_id, index=is_index, start=start_date, end=end_date, autype=None)
-df = df.sort_index(0)
-number_df = len(df)
-if number_df == 0:
-    raise SystemExit
-#print(df.head(10))
-
-#
-# Save to CSV File
-#
-df.to_csv('D:/DataCenter/PythonData/'+stock_id+'.csv')
+stock_number = len(stock_ids)
+for i in range(stock_number):
+    #
+    # Fetch Stock Data
+    #
+    stock_id = stock_ids[i]
+    is_index = is_indexs[i]
+    df = ts.get_h_data(stock_id, index=is_index, start=start_date, end=end_date, autype=None)
+    df = df.sort_index(0)
+    number_df = len(df)
+    if number_df == 0:
+        raise SystemExit
+    #print(df.head(10))
+    
+    #
+    # Save to CSV File
+    #
+    df.to_csv(path_datacenter+stock_id+'.csv')
