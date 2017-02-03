abbb44df39a4875aef279caa834a91de1d1d4d84
freefrom <freefrom_mn@163.com>
2017-02-04 00:16:50 +0800

    Add More File Constants.

--- Python/ConstantData.py
+++ Python/ConstantData.py
@@ -8,5 +8,7 @@ Created on Fri Feb  3 20:35:27 2017
 path_datacenter = '../DataCenter/'
 file_stockbasics = 'StockBasics.csv'
 file_quarterlyqfq = 'StockData_QuarterlyQFQ_%s.csv'
+file_financesummary = 'FinanceSummary_%s.csv'
+file_historicalpe = 'HistoricalPE_%s.xlsx'
 
 is_debug = True
